fb1c075a7d6368f518ad655de97fa3fea2d1d2ee
reemstar98 <reema.bali@outlook.com>
2019-10-14 22:19:47 +0930

    Add project files.

---
+++ E-Corp/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Corp.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
